bb74a98fc35fe7df925f0493a6562c25db94e7cf
Hunter Herman <hherman1@macalester.edu>
2017-02-20 10:56:39 -0600

    Report SOIL errors

--- src/Texture.cpp
+++ src/Texture.cpp
@@ -28,6 +28,13 @@ GLuint Texture::id() {
 // generates texture and its mipmaps. Supplied texture is forced to be level 0. 
 void Texture::loadImage(string filename) {
 	unsigned char* image = SOIL_load_image(filename.c_str(), &_width, &_height, 0, SOIL_LOAD_RGB);
+	if (image == NULL) {
+		SOIL_last_result();
+		std::runtime_error err(string("Error loading image: ") + string(SOIL_last_result()));
+		std::cerr << err.what();
+		//throw exception(err.what());
+		throw err;
+	}
 	glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, _width, _height,0,GL_RGB,GL_UNSIGNED_BYTE,image);
 	glGenerateMipmap(GL_TEXTURE_2D);
 	SOIL_free_image_data(image);
